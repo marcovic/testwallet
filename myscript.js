69732043b3fde8c56f39aa044e1961e63dbc9092
Marcovic Repo <hicsuntmarcovic@gmail.com>
2022-05-26 12:19:02 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,10 +1,4 @@
-let isOperaWallet = false;
-if(window.ethereum){
-    isOperaWallet = window.ethereum.isOpera;
-}
-if(isOperaWallet){
-    // add Opera Wallet as the first option in the wallet connection panel.
-}
+
 
 if(window.ethereum){
     window.ethereum.request({method: 'eth_requestAccounts'})
